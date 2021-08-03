63dddfd5c4e23afef4b230303a311347bcc1f67d
alanmz29 <47542106+alanmz29@users.noreply.github.com>
2021-08-03 17:39:18 -0400

    Progress on Program Managers Page

---
+++ Phase1/Team Budget Planner/ProgramManagersControl.js
@@ -0,0 +1,14 @@
+function getProjectInfo() {
+
+    var clientName = document.getElementById("clientName").value;
+    var projectName = document.getElementById("projectName").value;
+    var budget = document.getElementById("budget").value;
+
+}
+
+function clearProjectInfo() {
+
+    document.getElementById('clientName').value = '';
+    document.getElementById('projectName').value = '';
+    document.getElementById('budget').value = '';
+}
